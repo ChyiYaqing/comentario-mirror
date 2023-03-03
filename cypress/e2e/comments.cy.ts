e78a685cf008d42691ee9f5bad7ea1dc9ec8ed29
Dmitry Kann <yktooo@gmail.com>
2023-03-03 20:37:47 +0100

    e2e: baseUrl must be set to test-site for tests only

--- cypress/e2e/comments.cy.ts
+++ cypress/e2e/comments.cy.ts
@@ -3,11 +3,11 @@
 /** The base URL for the test site. */
 const baseUrl = Cypress.env('TEST_SITE_URL') || 'http://localhost:8000';
 
-context('Comments', {baseUrl}, () => {
+context('Comments', () => {
 
     before(cy.backendReset);
 
-    it('displays comments', () => {
+    it('displays comments', {baseUrl}, () => {
         cy.visit('/');
         cy.get('h1').should('have.text', 'This page has comments');
     });
